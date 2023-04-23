5831986344eba0251a0ae05af1a287c3bf22fce0
Jatin Gambhir <jatin.gambhir@jungleworks.com>
2023-04-23 23:15:03 +0530

    Added Logo Moving Animation

---
+++ logo-slide-animations/animation-1/script.js
@@ -0,0 +1,6 @@
+var copy = document.querySelector(".logos-slide-1").cloneNode(true);
+document.querySelector(".logos-1").appendChild(copy);
+var copy = document.querySelector(".logos-slide-2").cloneNode(true);
+document.querySelector(".logos-2").appendChild(copy);
+var copy = document.querySelector(".logos-slide-3").cloneNode(true);
+document.querySelector(".logos-3").appendChild(copy);
